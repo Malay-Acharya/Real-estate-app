4f05cbba24cb5c91a54379e4f8b94cfd9a5399fe
Malay <malayacharya21@gmail.com>
2022-07-29 20:46:41 +0530

    Navbar created

--- src/Components/Header/Nav.js
+++ src/Components/Header/Nav.js
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <>
+      <div className='flex flex-row justify-between mx-6 font-[14px] my-6'>
+        <div className='flex flex-row gap-4'>
+          <h3 className='font-bold mx-4 py-3 px-4 rounded-lg text-violet-400'>Estate World</h3>
+          <h3 className='hover:bg-violet-100 py-3 px-6 rounded-lg cursor-pointer'>Rent</h3>
+        </div>
+        <div className='flex flex-row gap-4'>
+          <button className='border-2 border-violet-100 py-3 px-6 rounded-lg hover:bg-violet-100'>Login</button>
+          <button className='border-2 border-violet-100 py-3 px-6 rounded-lg hover:bg-violet-100'>Sign up</button>
+        </div>
+      </div>
+      <hr/>
+    </>
   )
 }
